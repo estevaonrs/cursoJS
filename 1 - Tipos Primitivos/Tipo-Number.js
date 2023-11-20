d2cc6e017fbdb287c1476e145210c70fd0defbfc
estevaonrs <tevonrs@gmail.com>
2023-11-20 17:22:12 -0300

    curso

---
+++ 1 - Tipos Primitivos/Tipo-Number.js
@@ -0,0 +1,29 @@
+// Tipo Number
+
+const meuNumero = 3;
+const primeiroNumero = 1;
+const segundoNumero = 2;
+const operacaoMatematica = primeiroNumero / segundoNumero;
+
+console.log(operacaoMatematica)
+
+
+// Ponto Flutuante
+
+const numeroPontoFlutuante = 3.3;
+const numeroPontoFlutuante2 = .5;
+const NovaOperacao = numeroPontoFlutuante / numeroPontoFlutuante2;
+
+console.log(NovaOperacao)
+
+
+// NaN -> Not A Number
+
+const alura = "Alura";
+const numeroNAN = 1;
+const OperacaoNAN = alura * numeroNAN;
+
+console.log(OperacaoNAN)
+
+
+
